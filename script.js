02429b4ca22e9aeff83dad013c5bf2bad168781f
steliviere <32293736+steliviere@users.noreply.github.com>
2018-11-19 13:23:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello, world!")
+$(document).ready(function(){
+	$("#intro_").on("click",function(event){
+		var offset = $("#intro").offset();
+		$("html body").animate({scrollTop:offset.top},500);
+	});
+	$("#story_").on("click",function(event){
+		var offset = $("#story").offset();
+		$("html body").animate({scrollTop:offset.top},500);
+	});
+	$("#monument_").on("click",function(event){
+		var offset = $("#monument").offset();
+		$("html body").animate({scrollTop:offset.top},500);
+	});
+});
+$(window).scroll(function() {
+	var el = $('ul');
+	var offset = $("#story").offset();
+	console.log(offset);
+	if($(this).scrollTop() <= offset.top) el.addClass('white');
+	else $el.removeClass('white');
+});
