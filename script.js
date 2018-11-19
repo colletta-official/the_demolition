dca94b7ca870aecb7a47431587e4f297ac85ec23
steliviere <32293736+steliviere@users.noreply.github.com>
2018-11-19 14:24:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,37 @@ $(window).scroll(function() {
 	if($(this).scrollTop() < offset.top) $el.addClass('white');
 	else $el.removeClass('white');
 });
+window.onload = function () {
+	$("section").each(function () {
+		$(this).on("mousewheel DOMMouseScroll", function (e) {
+			e.preventDefault();
+			var delta = 0;
+			if (!event) event = window.event;
+			if (event.wheelDelta) {
+				delta = event.wheelDelta / 120;
+				if (window.opera) delta = -delta;
+			}
+			else if (event.detail) delta = -event.detail / 3;
+			var moveTop = null;
+			if (delta < 0) {
+				if ($(this).next() != undefined) {
+					moveTop = $(this).next().offset().top;
+				}
+			}
+			else {
+				if ($(this).prev() != undefined) {
+					moveTop = $(this).prev().offset().top;
+				}
+			}
+			$("html,body").stop().animate(
+			{
+				scrollTop: moveTop + 'px'
+				}, {
+				duration: 500, complete: function () {}
+			});
+		});
+	});
+}
 function linkmove(where)
 {
 	var offset = $("#" + where).offset();
